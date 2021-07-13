26dcbad1765ef611b1f8951f369432ddbb2a07aa
ukitomo <tomoemon_u@outlook.jp>
2021-07-13 14:54:51 +0900

    Revert "www"
    This reverts commit ae733f9b9047d0f802eebbe9f324627a5dd47c27.

--- en/footer.php
+++
@@ -1,3 +0,0 @@
-<!DOCTYPE html>
-<div class="copyright"><p>&copy; 2019-2021 ukitomo</p style="color:#fff;"></div>
-<div class="back-to-top"><a href="#"><span class="button">back-to-top</span></a></div>
--- footer.php
+++
@@ -1,3 +0,0 @@
-<!DOCTYPE html>
-<div class="copyright"><p style="color:#fff;">&copy; 2019-2021 ukitomo</p></div>
-<div class="back-to-top"><a href="#"><span class="button">トップへ戻る</span></a></div>
